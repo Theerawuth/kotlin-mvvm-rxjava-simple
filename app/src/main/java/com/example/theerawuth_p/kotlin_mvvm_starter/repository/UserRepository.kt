83e11701b44eee6aedddcdffcb888790ea3d6aa2
Theerawuth_P <Theerawuth_Pat@truecorp.co.th>
2017-10-18 15:23:04 +0700

    Drop memory cache

--- app/src/main/java/com/example/theerawuth_p/kotlin_mvvm_starter/repository/UserRepository.kt
+++ app/src/main/java/com/example/theerawuth_p/kotlin_mvvm_starter/repository/UserRepository.kt
@@ -16,39 +16,35 @@ class UserRepository(val userApi: UserApi, val userDao: UserDao) {
     var cachedUsers = emptyList<User>()
 
     fun getUsers(): Observable<List<User>> {
-        return Observable.concatArrayDelayError(
-                getUsersFromCache(), getUsersFromDb(), getUsersFromApi())
-    }
-
-    fun getUsersFromCache(): Observable<List<User>> {
-        return Observable.just(cachedUsers).filter { it.isNotEmpty() }
-                .doOnNext { Timber.d("Dispatching ${it.size} users from Cache.") }
+        return Observable.concatArray(
+                getUsersFromDb(),
+                getUsersFromApi())
     }
 
     fun getUsersFromDb(): Observable<List<User>> {
         return userDao.getUsers().filter { it.isNotEmpty() }
                 .toObservable()
                 .doOnNext {
-                    Timber.d("Dispatching ${it.size} users from DB.")
+                    Timber.d("Dispatching ${it.size} users from DB..")
                 }
     }
 
     fun getUsersFromApi(): Observable<List<User>> {
         return userApi.getUsers()
                 .doOnNext {
-                    Timber.d("Dispatching ${it.size} users from API.")
-                    cacheUsers(it)
+                    Timber.d("Dispatching ${it.size} users from API..")
+                    storeUsersInDb(it)
                 }
     }
 
-    fun cacheUsers(users: List<User>) {
+    fun storeUsersInDb(users: List<User>) {
         Timber.d("Cached ${users.size} users in memory.")
         cachedUsers = users
         Observable.fromCallable { userDao.insertAll(users) }
                 .subscribeOn(Schedulers.io())
                 .observeOn(Schedulers.io())
                 .subscribe {
-                    Timber.d("Inserted ${users.size} users from API in DB.")
+                    Timber.d("Inserted ${users.size} users from API in DB...")
                 }
     }
 
--- app/src/main/java/com/example/theerawuth_p/kotlin_mvvm_starter/viewmodel/UserListViewModel.kt
+++ app/src/main/java/com/example/theerawuth_p/kotlin_mvvm_starter/viewmodel/UserListViewModel.kt
@@ -4,6 +4,7 @@ import com.example.theerawuth_p.kotlin_mvvm_starter.repository.UserRepository
 import com.example.theerawuth_p.kotlin_mvvm_starter.viewmodel.data.UsersList
 import io.reactivex.Observable
 import timber.log.Timber
+import java.util.concurrent.TimeUnit
 
 /**
  * Created by theerawuth_p on 10/17/2017 AD.
@@ -13,6 +14,9 @@ class UserListViewModel(val userRepository: UserRepository) {
     fun getUsers(): Observable<UsersList> {
         // Create the data for your UI, the users lists and maybe some additional data need as well
         return userRepository.getUsers()
+                // Drop DB data if we can fetch item enough from the API
+                // to avoid UI Flickers
+                .debounce(400, TimeUnit.MILLISECONDS)
                 .map {
                     Timber.d("Mapping users to UIData...")
                     UsersList(it.take(10), "Top 10 Users")
